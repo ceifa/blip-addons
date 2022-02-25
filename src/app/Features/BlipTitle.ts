3b81a0a84d4b8f6dafa7d0d819319ae90f7070ff
Jota <jjunior@protonmail.ch>
2022-02-25 18:34:01 -0300

    feat: dynamic title feature

---
+++ src/app/Features/BlipTitle.ts
@@ -0,0 +1,29 @@
+import { BaseFeature } from './BaseFeature'
+
+export class BuilderTitle extends BaseFeature {
+  public get botName() {
+    const botNameElement = document.querySelector('.bot-name:nth-child(1)')
+
+    return botNameElement.innerHTML
+  }
+
+  public get isBeholder() {
+    return Array.from(document.querySelectorAll('iframe')).find((iFrame) =>
+      iFrame.getAttribute('src').includes('beholder')
+    )
+  }
+
+  public handle() {
+    const botName = this.botName
+
+    document.title = `${botName} - Blip`
+  }
+
+  public cleanup() {
+    if (this.isBeholder) {
+      document.title = 'Beholder - Blip'
+    } else {
+      document.title = 'Blip Portal'
+    }
+  }
+}
